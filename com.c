07e3b741bfe95eb626d1d5e10beb672c2725f877
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-02-09 16:45:14 +0530

    com.c

---
+++ com.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[10]="hello";
+    char str1[10]="world";
+    int i,j;
+    scanf("%c",&str[i]);
+    scanf("%c",&str1[j]);
+   for(i=0;str!='\0';i++)
+   {
+   for(j=0;str1!='\0';j++)
+   {
+       printf("%c",str1[j]);
+   }
+   }
+return 0;
+}
