475262e6bec4f9a43fd6a34e5923a3965423d883
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-02-12 16:03:57 +0530

    lows.c

---
+++ lows.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a[10]={5,7,4,8},i;
+for(i=0;i<=3;i++)
+{
+if(a[i]>7)
+{
+printf("print the value of p %d\n",a[i]);
+}
+else if(a[i]<5)
+{
+printf("print the value of p %d\n",a[i]);
+}
+else
+{
+    printf("\n");
+}
+}
+return 0;
+}
