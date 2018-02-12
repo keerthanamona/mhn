ada9fa6f2f3bce22f399ca52d238c47d0a94d71e
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-02-12 16:26:55 +0530

    gap.c

---
+++ gap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a[10]={1,2,3},i;
+    for(i=0;i<=0;i++)
+    {
+        
+       
+    printf("%d\t%d\t%d",a[0],a[1],a[2]);
+        
+    }
+    return 0;
+    
+    
+}
