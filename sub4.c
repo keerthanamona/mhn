f13ab05615c5ab55ed9294144c3580c279c0f3aa
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-02-12 16:45:22 +0530

    sub4.c

---
+++ sub4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int m;
+    printf("enter the number");
+    scanf("%d",&m);
+    if(m>0)
+    {
+        m=m-1;
+    printf("sub of value%d",m);
+    }
+    return 0;
+}
