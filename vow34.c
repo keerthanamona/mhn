a6e3d648a6c3af7fd96a5e29761e57e7e74ab47e
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-02-26 16:15:09 +0530

    vow34.c

---
+++ vow34.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[5]={"apol"},i=0;
+if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
+{
+printf("it is a vowel");
+}
+else
+{
+printf("it is not vowel");
+}
+return 0;
+}
