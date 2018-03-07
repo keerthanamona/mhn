fbcd4dc46d154260c2b488a1453e6dbf30937a24
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-03-07 15:57:10 +0530

    t.c

---
+++ t.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+	int n,i,b,e,c;
+	printf("enter the number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		b=pow(2,i);
+		if(a>n)
+		{
+			e=i;
+			break;
+		}
+	}
+	c=pow(2,e);
+	printf("\n%d",c);
+	
+	return 0;
+}
