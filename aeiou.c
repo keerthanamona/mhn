1c42d16b7f44bc2633b478c61db0449a8da2d8dc
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-03-16 16:41:51 +0530

    aeioc.c

---
+++ aeiou.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[20]={"apple"};
+int n,i;
+n=strlen(str);
+for(i=0;i<n;i++)
+{
+if(str[i]!='a'&&str[i]!='e')
+{
+    else if(str[i]!='i')&&(str[i]!='o')
+    {
+        else if(str[i]!='u')
+{
+printf("%s",str[i]);
+}
+}
+}
+return 0;
+}
