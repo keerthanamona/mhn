7f41a9630fb0279dd0e89740fb271d57280ace13
keerthanamona <30549472+keerthanamona@users.noreply.github.com>
2018-03-16 15:55:57 +0530

    str56.c

---
+++ str56.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[4]={"aab"};
+char b[4]={"aay"};
+int i;
+for(i=0;i<=0;i++)
+{
+if(a[i]==b[i])
+{
+printf("no");
+}
+else
+{
+printf("yes");
+}
+}
+return 0;
+}
